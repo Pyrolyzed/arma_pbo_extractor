b1f382ba627885ce74e21402e36fd30bf03c7b30
Pyrolyzed <pyrolyzed@proton.me>
2026-06-25 17:35:55 -0500

    Lots of changes

--- arma_auto_pbo_extractor.py
+++ arma_auto_pbo_extractor.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 import os
 import os.path
 import shutil
+import time
 
 from PySide6.QtWidgets import (
     QApplication, 
@@ -14,27 +15,16 @@ from PySide6.QtWidgets import (
 )
 app = QApplication()
 def choose_directories(base:Path = Path('.')) -> Optional[List[str]]:
-    """
-    Open a dialogue to select multiple directories
-    Args:
-        base (Path): Starting directory to show when opening dialogue
-    Returns:
-        List[str]: List of paths that were selected, ``None`` if "cancel" selected"
-    References:
-        Mildly adapted from https://stackoverflow.com/a/28548773
-        to use outside an exising Qt Application
-    """
     file_dialog = QFileDialog()
     file_dialog.setOption(QFileDialog.Option.DontUseNativeDialog, True)
     file_dialog.setFileMode(QFileDialog.Directory)
     for widget_type in (QListView, QTreeView):
         for view in file_dialog.findChildren(widget_type):
             if isinstance(view.model(), QFileSystemModel):
-                view.setSelectionMode(
-                    QAbstractItemView.ExtendedSelection)
+                view.setSelectionMode(QAbstractItemView.ExtendedSelection)
 
     if file_dialog.exec():
-        paths = file_dialog.selectedFiles()
+        paths = list(map(Path, file_dialog.selectedFiles()))
         return paths
 
 def get_pbo_list(folder_path: str) -> List[str]:
@@ -48,30 +38,56 @@ def get_pbo_list(folder_path: str) -> List[str]:
         return pbo_list
     return []
 
+def get_sizes(file_list: List[str]) -> str:
+    size: int = 0
+    for file in file_list:
+        size += os.path.getsize(file)
+    return str(f"{float(size / (1024 ** 3)):.2f}GB")
+
 def copy_files(destination_path: Path, file_list: list[str]):
     for file in file_list:
-        shutil.copy(file, destination_path)
+        print(f">  Copying {file.split("\\")[-1]} to {str(destination_path)}")
+        try:
+            shutil.copy(file, destination_path)
+        except shutil.SameFileError:
+            print(f"Failed due to duplicate file {file.split("\\")[-1]}.")
+        except:
+            print("Failed due to a generic error.")
 
 def main():
     print("Asking for destination directory...")
     destination: Path = Path(choose_directories()[0])
     print(f"Destination directory: {str(destination)}")
+
     print("\nAsking for mod directories...")
     mod_directories: List[str] = choose_directories()
+
     print(f"\nYou selected {len(mod_directories)} directories.")
     [print(mod_directory) for mod_directory in mod_directories]
+
+    pbo_list = []
+    for mod_directory in mod_directories:
+        for file in get_pbo_list(mod_directory):
+            pbo_list.append(file)
+
+    print(f"Found {len(pbo_list)} PBO files.")
+    print(f"Total Size: {get_sizes(pbo_list)}")
+    total, used, free = shutil.disk_usage(str(destination).split("\\")[0] + "\\")
+    print(f"Free Space: {free // (2**30)} GiB")
+
     should_continue: bool = False if input("\nContinue? [Y/n]\n").lower().startswith("n") else True
+
     if not should_continue:
         print("Exiting.")
         exit(0)
 
-    counter = 0
-    for mod_directory in mod_directories:
-        pbo_list = get_pbo_list(mod_directory)
-        counter += len(pbo_list)
-        copy_files(destination, pbo_list)
-    print(f"Done! Copied {counter} files.") 
+    print(f"\nCopying files to {str(destination).split("\\")[-1]}...")
 
+    start_time = time.time()
+
+    copy_files(destination, pbo_list)
+
+    print(f"\n\n\033[93mDone in {(time.time() - start_time) / 60} minutes!")
 
 if __name__ == "__main__":
     main()
